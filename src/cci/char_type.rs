a6291f5e0b1051d8662073232764d7a7a3223acf
Koji Ota <afterjnih@gmail.com>
2017-04-15 01:48:36 +0900

    wip

--- src/cci/char_type.rs
+++ src/cci/char_type.rs
@@ -1,3 +1,5 @@
+#[derive(Debug)]
+#[derive(PartialEq)]
 enum CharType {
     Lparen,
     Rparen,
@@ -19,31 +21,10 @@ enum CharType {
     Comma,
     SngQ,
     DblQ,
-    Void,
-    Int,
-    If,
-    Else,
-    For,
-    While,
-    Do,
-    Switch,
-    Case,
-    Equal,
-    NotEq,
     Less,
-    LessEq,
     Great,
-    GreatEq,
-    And,
-    Or,
-    EndKeyList,
-    Idnet,
-    IntNum,
-    String,
-    NulKind,
     Letter,
     Digit,
-    EofTkn,
     Others,
 }
 
@@ -54,9 +35,50 @@ trait CharTypable {
 impl CharTypable for char {
     fn char_type(&self) -> CharType {
         match self {
-            &'{' => CharType::Lparen,
-            &'}' => CharType::Rparen,
+            &'(' => CharType::Lparen,
+            &')' => CharType::Rparen,
+            &'{' => CharType::Lbrace,
+            &'}' => CharType::Rbrace,
+            &'[' => CharType::Lbracket,
+            &']' => CharType::Rbracket,
+            &'+' => CharType::Plus,
+            &'-' => CharType::Minus,
+            &'*' => CharType::Multi,
+            &'/' => CharType::Divi,
+            &'%' => CharType::Mod,
+            &'!' => CharType::Not,
+            &':' => CharType::Colon,
+            &';' => CharType::Semicolon,
+            &'=' => CharType::Assign,
+            &'#' => CharType::Sharp,
+            &'\\' => CharType::Yen,
+            &',' => CharType::Comma,
+            &'\'' => CharType::SngQ,
+            &'"' => CharType::DblQ,
+            &'<' => CharType::Less,
+            &'>' => CharType::Great,
+            &'a'...'z' => CharType::Letter,
+            &'A'...'Z' => CharType::Letter,
+            &'0'...'9' => CharType::Digit,
             _ => CharType::Others,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::CharTypable;
+    use super::CharType;
+    #[test]
+    fn test_char_type() {
+        assert_eq!('('.char_type(), CharType::Lparen);
+        assert_eq!('\''.char_type(), CharType::SngQ);
+        assert_eq!('\\'.char_type(), CharType::Yen);
+        assert_eq!('a'.char_type(), CharType::Letter);
+        assert_eq!('z'.char_type(), CharType::Letter);
+        assert_eq!('A'.char_type(), CharType::Letter);
+        assert_eq!('Z'.char_type(), CharType::Letter);
+        assert_eq!('0'.char_type(), CharType::Digit);
+        assert_eq!('9'.char_type(), CharType::Digit);
     }
 }
